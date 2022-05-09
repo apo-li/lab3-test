15f57c648a88023735d33edbcef0d22b8f1b24d9
Apo <apistolos@yahoo.gr>
2022-05-09 15:00:36 +0300

    slightly different now

--- lab03/src/main/java/lab03/MyMain.java
+++ lab03/src/main/java/lab03/MyMain.java
@@ -2,6 +2,8 @@ package lab03;
 
 public class MyMain {
 	public static void main(String[] args) {
+		System.out.println("this is now slightly different");
 		System.out.println("Hello World");
 	}
 }
+
